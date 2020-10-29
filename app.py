2f6f4e163ae727f0e38332adb6f2ade6583811e3
jcpoulson <60674086+jcpoulson@users.noreply.github.com>
2020-10-29 16:56:26 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -16,8 +16,20 @@ class Product(Model):
 def read():
     with open('inventory.csv', 'r') as file:
         for line in file:
-            new_line = line.split('')
-            print(new_line)
+            if '"' in line:
+                """This If statement cleans up the lines that have commas in the product_name section"""
+                new_line_1 = line[1:]
+                new_line_2 = new_line_1.split('"')
+                new_line_3 = new_line_2[1][1:]
+                new_line_4 = new_line_3.split(",")
+                new_list = []
+                new_list.append(new_line_2[0])
+                for value in new_line_4:
+                    new_list.append(value)
+                print(new_list)
+            else:
+                new_line = line.split(",")
+                print(new_line)
 
 if __name__ == '__main__':
     db.connect()
